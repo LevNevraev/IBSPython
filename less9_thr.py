1e1a3fc045f7cf511aa8e5461ba43879f943436a
LevNevraev <84562148+LevNevraev@users.noreply.github.com>
2021-09-21 11:55:39 +0300

    Update less9_thr.py

--- less9_thr.py
+++ less9_thr.py
@@ -2,27 +2,25 @@ import threading
 import time
 
 
-def run1(interval):
-    i = 10
-    while i > 0:
-        print(f'Time: {time.ctime()} Thread: {t.getName()}', i)
-        time.sleep(interval)
-        i = i - 1
+class ClockThread(threading.Thread):
+    def __init__(self, interval):
+        super().__init__()
+        self.daemon = True
+        self.interval = interval
+
+    def run(self):
+        print(self.getName(), 'начал выполнение')
+        i = 10
+        while i > 0:
+            print(self.getName(), i)
+            time.sleep(self.interval)
+            i = i - 1
+        print(self.getName(), 'закончил выполнение')
 
 
-def run2(interval):
-    i = 10
-    while i > 0:
-        print(f'Time: {time.ctime()} Thread: {t2.getName()}', i)
-        time.sleep(interval)
-        i = i - 1
-
-
-t = threading.Thread(target=run1, args=(1,))
-t.daemon = True
-t.start()
-t2 = threading.Thread(target=run2, args=(1,))
-t2.daemon = True
+t1 = ClockThread(1)
+t2 = ClockThread(1)
+t1.start()
 t2.start()
-t.join()
+t1.join()
 t2.join()
